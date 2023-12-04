1c2d3f65c9c63fe55b4e2358ccf4d210671a555e
LeifMujeh <Leif.abumujeh@elev.ga.ntig.se>
2023-12-04 10:30:43 +0100

    test

--- Program.cs
+++ Program.cs
@@ -1,16 +1,3 @@
 ﻿using System;
 using Raylib_cs;
 
-Raylib.InitWindow(800, 600, "Raylib");
-Raylib.SetTargetFPS(60);
-
-
-
-
-while (!Raylib.WindowShouldClose())
-{
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.WHITE);
-    
-    Raylib.EndDrawing();
-}
