c8e0b1cff6d73a0de54eb0a11ded127eead765d3
LeifMujeh <Leif.abumujeh@elev.ga.ntig.se>
2024-01-23 14:06:57 +0100

    fixat tilmapen ratio

--- Map.cs
+++ Map.cs
@@ -5,14 +5,17 @@ using Raylib_cs;
 
 public class Map
 {
-    private int[,] tileMap = new int[8, 11] {
+    private int[,] tileMap = new int[11, 11] {
         {1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0,},
         {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
         {0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0},
         {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
         {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1},
-        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1},
+        {0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
         {0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0},
     };
 
@@ -38,18 +41,20 @@ public class Map
         //    for (int y = 0; y < tileMap.GetLength(0); y++){
         //for (int x = 0; x < tileMap.GetLength(1); x++){
 
-        for (int row = 0; row < 8; row++)
+        for (int row = 0; row < 11; row++)
         {
             for (int col = 0; col < 11; col++)
             {
                 Rectangle tileRect = new Rectangle(row * 75, col * 75, 75, 75);
                 if (this.tileMap[row, col] == 1)
                 {
-                    Raylib.DrawRectangleRec(tileRect, Color.Black);
+                    Raylib.DrawRectangleRec(tileRect, Color.DarkGreen);
                 }
 
                 else
+                {
                     Raylib.DrawRectangleRec(tileRect, Color.Green);
+                }
             }
         }
     }
